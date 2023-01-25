a0b0c47cb3757227bd86eb1620a04ea61e8d036f
Marcos Cardenas-Zelaya <marcos.cardenaszelaya@parivedasolutions.com>
2023-01-25 14:32:09 -0500

    add bad code again

---
+++ bad.py
@@ -0,0 +1,11 @@
+unused_var = 5
+unused_var = 5
+unused_var = 5
+
+
+def is_number_even(num):
+    return "Even" if num % 2 == 0 else "Odd"
+
+
+num = 5
+print(f"The number {num} is {is_number_even(num)}")
